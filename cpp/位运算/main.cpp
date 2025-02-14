829d118e0ad505a9cbd32c84c679b662e0782a17
nezha <2099868853@qq.com>
2025-02-14 17:37:32 +0800

    更新位运算和枚举

--- cpp/位运算/main.cpp
+++ cpp/位运算/main.cpp
@@ -62,22 +62,108 @@
  
 
 //力扣，190.颠倒二进制
+//class Solution 
+//{
+//public:
+//    uint32_t reverseBits(uint32_t n) 
+//	{
+//		uint32_t ret = 0;
+//        for(int i = 0; i < 32; i++)
+//        {
+//        	uint32_t tmp = (n>>i) & 1;
+//        	tmp <<= (31 - i);
+//        	ret |= tmp;
+//        	
+//		}
+//        return ret;
+//    }
+//};
+
+
+//力扣，476.数字的补数 
+//class Solution 
+//{
+//public:
+//    int findComplement(int num)
+//    {
+//    	int i = 0;//num第几位
+//		int ret = 0; 
+//        while(num)
+//        {
+//        	if((num & 1) == 0)
+//        		ret |= (1 << i);
+//        	i++;
+//        	num >>= 1;
+//		}
+//		return ret;
+//    }
+//};
+
+
+
+//力扣，191.位1的个数 
+//class Solution
+//{
+//public:
+//    int hammingWeight(int n) 
+//	{
+//		int count = 0;
+//        while(n)
+//        {
+//        	n = n & (n-1);
+//        	count++;
+//		}
+//		return count;
+//    }
+//};
+
+
+//力扣，231.2的幂
+//class Solution 
+//{
+//public:
+//    bool isPowerOfTwo(int n) 
+//	{
+//		return (n>=1) && (n&(n-1) == 0);
+//    }
+//};
+
+
+//力扣，136.只出现一次的数字
+//class Solution 
+//{
+//public:
+//    int singleNumber(vector<int>& nums) 
+//	{
+//		int ret = 0;
+//        for(auto x : nums)
+//        {
+//        	ret ^= x;
+//		}
+//		return ret;
+//    }
+//};
+
+
+//力扣，268.丢失的数字 
 class Solution 
 {
 public:
-    uint32_t reverseBits(uint32_t n) 
+    int missingNumber(vector<int>& nums) 
 	{
-		uint32_t ret = 0;
-        for(int i = 0; i < 32; i++)
-        {
-        	uint32_t tmp = (n>>i) & 1;
-        	tmp <<= (31 - i);
-        	ret |= tmp;
-        	
-		}
-        return ret;
+		int ret = 0;
+        for(int i = 0; i < nums.size(); i++)
+		{
+			ret ^= nums[i];
+			ret ^= i;
+		} 
+		return ret ^ nums.size();
     }
 };
+
+
+
+
 
 
 
--- 算法基础/1.基础算法/3.枚举/main.cpp
+++ 算法基础/1.基础算法/3.枚举/main.cpp
@@ -90,7 +90,26 @@
 
 
 //力扣， 78子集 
-
+class Solution 
+{
+public:
+    vector<vector<int>> subsets(vector<int>& nums) 
+	{
+        vector<vector<int>> ret;
+        int n = nums.size();
+		for(int i = 0; i < (1 << n); i++)//i：二进制数 
+		{
+			vector<int> tmp;
+			for(int j = 0; j < n; j++)//j:位数
+			{
+				if((i >> j) & 1)	tmp.push_back(nums[j]);
+				
+			}
+			ret.push_back(tmp);
+		} 
+		return ret;
+    }
+};
 
 
 
